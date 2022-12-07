448c3907a994d7ff6a03177afd9ed71bfd9736b8
Coxine <59347549+coxine@users.noreply.github.com>
2022-12-07 14:11:53 +0800

    1st commit

---
+++ script.js
@@ -0,0 +1,84 @@
+var dialog = document.querySelector('.dialog');
+        var url = document.getElementById('url');
+        var shortStr = document.getElementById('shortStr');
+        var submit = document.getElementById('submit');
+        submit.onclick = function () {
+            if (url.value == '') {
+                alert('请输入长链接');
+                return false;
+            }
+            fetch('/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    url: url.value,
+                    shortStr: shortStr.value
+                })
+            }).then(function (res) {
+                if (res.ok)
+                    return res.json();
+                else
+                    throw res;
+            }).then(function (data) {
+                // alert('提交成功，短链接为：' + data.data.shortUrl);
+                // 在弹窗上显示短链接
+
+                dialog.querySelector('.dialog-body').innerHTML =
+                    `<p>短链接：<span id="shortUrl">${location.host}/${data.data.shortUrl}</span></p>`;
+                // 在弹窗上显示复制按钮
+                dialog.querySelector('.dialog-footer').innerHTML =
+                    `<button class="copy" id="copy">复制</button>`;
+
+                // 复制按钮
+                var copy = document.getElementById('copy');
+                copy.onclick = function () {
+                    // 将短链接复制到剪贴板
+                    var shortUrl = document.querySelector('#shortUrl');
+                    try {
+                        let range = document.createRange();
+                        range.selectNode(shortUrl);
+                        window.getSelection().removeAllRanges();
+                        window.getSelection().addRange(range);
+                        document.execCommand('copy');
+                        window.getSelection().removeAllRanges();
+                        console.log('Copy success')
+                    } catch (e) {
+                        console.log('Copy error')
+                    }
+                }
+                // 显示弹窗
+                dialog.classList.add('dialog-show');
+            }).catch(async function (err) {
+                console.log(err);
+                let data;
+                try {
+                    data = await err.json();
+                } catch (e) {
+                    alert('提交失败，请检查网络');
+                    return false
+                }
+                console.log(data);
+                dialog.querySelector('.dialog-body').innerHTML = `<p>${data.msg}</p>`;
+                dialog.querySelector('.dialog-footer').innerHTML =
+                    `<button class="conform" id="conform">确定</button>`;
+                var conform = document.getElementById('conform');
+                conform.onclick = function () {
+                    dialog.classList.remove('dialog-show');
+                }
+                dialog.classList.add('dialog-show');
+            })
+        }
+        // 关闭按钮
+        document.querySelector('.dialog-close').onclick = function () {
+            dialog.classList.remove('dialog-show');
+        }
+        // 点击空白处关闭弹窗
+        dialog.onclick = function (e) {
+            // 阻止事件冒泡
+            e.stopPropagation();
+            if (e.target == dialog) {
+                dialog.classList.remove('dialog-show');
+            }
+        }
