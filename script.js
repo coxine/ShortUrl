a09c17e3dad90d9fd54e272bb0a10e8565ed7b57
coxine <59347549+coxine@users.noreply.github.com>
2023-09-05 21:00:15 +0800

    UI improvement on input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var dialog = document.querySelector('.dialog');
+var result = document.querySelector('.result');
         var url = document.getElementById('url');
         var shortStr = document.getElementById('shortStr');
         var submit = document.getElementById('submit');
@@ -25,10 +25,10 @@ var dialog = document.querySelector('.dialog');
                 // alert('提交成功，短链接为：' + data.data.shortUrl);
                 // 在弹窗上显示短链接
 
-                dialog.querySelector('.dialog-body').innerHTML =
+                result.querySelector('.result-body').innerHTML =
                     `<p>短链接：<span id="shortUrl">${location.host}/${data.data.shortUrl}</span></p>`;
                 // 在弹窗上显示复制按钮
-                dialog.querySelector('.dialog-footer').innerHTML =
+                result.querySelector('.result-footer').innerHTML =
                     `<button class="copy" id="copy">复制</button>`;
 
                 // 复制按钮
@@ -49,7 +49,7 @@ var dialog = document.querySelector('.dialog');
                     }
                 }
                 // 显示弹窗
-                dialog.classList.add('dialog-show');
+                result.classList.add('result-show');
             }).catch(async function (err) {
                 console.log(err);
                 let data;
@@ -60,25 +60,25 @@ var dialog = document.querySelector('.dialog');
                     return false
                 }
                 console.log(data);
-                dialog.querySelector('.dialog-body').innerHTML = `<p>${data.msg}</p>`;
-                dialog.querySelector('.dialog-footer').innerHTML =
+                result.querySelector('.result-body').innerHTML = `<p>${data.msg}</p>`;
+                result.querySelector('.result-footer').innerHTML =
                     `<button class="conform" id="conform">确定</button>`;
                 var conform = document.getElementById('conform');
                 conform.onclick = function () {
-                    dialog.classList.remove('dialog-show');
+                    result.classList.remove('result-show');
                 }
-                dialog.classList.add('dialog-show');
+                result.classList.add('result-show');
             })
         }
         // 关闭按钮
-        document.querySelector('.dialog-close').onclick = function () {
-            dialog.classList.remove('dialog-show');
+        document.querySelector('.result-close').onclick = function () {
+            result.classList.remove('result-show');
         }
         // 点击空白处关闭弹窗
-        dialog.onclick = function (e) {
+        result.onclick = function (e) {
             // 阻止事件冒泡
             e.stopPropagation();
-            if (e.target == dialog) {
-                dialog.classList.remove('dialog-show');
+            if (e.target == result) {
+                result.classList.remove('result-show');
             }
         }
